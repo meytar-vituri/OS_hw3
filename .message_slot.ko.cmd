518b1240e7ffecfaf54b477d6a0d74d3690f567a
student <meytarvituri@mail.tau.ac.il>
2020-12-17 11:11:50 +0200

    first remove

--- .message_slot.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/student/CLionProjects/OS3/message_slot.ko := ld -r -m elf_x86_64  -z max-page-size=0x200000 -T ./scripts/module-common.lds  --build-id  -o /home/student/CLionProjects/OS3/message_slot.ko /home/student/CLionProjects/OS3/message_slot.o /home/student/CLionProjects/OS3/message_slot.mod.o ;  true
